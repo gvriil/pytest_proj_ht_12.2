c428d2d20c09ad0e1ab7f8cc911bd3a7e0cbfbc5
G <0020992@gmail.com>
2023-08-27 21:49:55 +0300

    "we are dane with TDD (Test Driven Development) and 100% covarage of get_val function"

---
+++ tests/test_get_val.py
@@ -0,0 +1,8 @@
+import pytest
+from utils.dict_get_val import get_val
+
+def test_get_val ():
+    assert get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
+    assert get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+    assert get_val({}, "vcs", "git") == "git"
+    assert get_val({}, "vcs", "bazaar") == "bazaar"
