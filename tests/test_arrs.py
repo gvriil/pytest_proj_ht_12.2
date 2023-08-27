494122d8c4b45253c04c88fa204efc511b4f2160
G <0020992@gmail.com>
2023-08-27 19:34:44 +0300

    "code coverage is 100%"

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -8,4 +8,9 @@ def test_get():
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], 1) == [2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, 4], 3, 1) == []
+    assert arrs.my_slice([1, 2, 3, 4], 1, -1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], 1, 5) == [2, 3, 4]
+    assert arrs.my_slice([], 0, 0) == []
+    assert arrs.my_slice([1, 2, 3, 4]) == [1, 2, 3, 4]
